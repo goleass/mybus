fb5c03429e382ad8482672a51cc70072f9391bb0
Leonardo Assunção <leonardo.assuncao@upperds.com>
2022-12-01 17:31:32 -0300

    Create path

--- src/pages/acompanhamentoOnibus.jsx
+++ src/pages/acompanhamentoOnibus.jsx
@@ -1,15 +1,18 @@
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { DirectionsRenderer, GoogleMap, Polyline, useJsApiLoader } from '@react-google-maps/api';
 import { Marker } from '@react-google-maps/api';
 import { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom"
 import axios from "axios"
 
 import myBusIcon from '../assets/64284.png'
+import myBusStop from '../assets/64286.png'
+import { Directions } from '@mui/icons-material';
 
 function AcompanhamentoOnibus() {
 
   const { codItinerario } = useParams()
   const [coordenadasOnibus, setCoordenadasOnibus] = useState([])
+  const [coordenadasParadas, setCoordenadasParadas] = useState([])
   const [myLocation, setMyLocation] = useState({
     lat: -29.923990, lng: -51.170316
   })
@@ -19,11 +22,21 @@ function AcompanhamentoOnibus() {
     googleMapsApiKey: import.meta.env.VITE_API_KEY
   })
 
+  const buscaCoordenadasParadas = async () => {
+    try {
+      const { data } = await axios.get(`${import.meta.env.VITE_BUS_API}/coordenadas-intinerario?codItinerario=${codItinerario}`)
+
+      setCoordenadasParadas(data.map(c => ({ lat: c.coordY, lng: c.coordX })))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   const buscaCoordenadasOnibus = async () => {
     try {
       const { data } = await axios.get(`${import.meta.env.VITE_BUS_API}/coordenadas-onibus?codItinerario=${codItinerario}`)
 
-      setCoordenadasOnibus(data.map(c => ({lat: c.lat, lng: c.long})))
+      setCoordenadasOnibus(data.map(c => ({ lat: c.lat, lng: c.long })))
     } catch (error) {
       console.log(error)
     }
@@ -31,7 +44,8 @@ function AcompanhamentoOnibus() {
 
   useEffect(() => {
     const coordsByLS = JSON.parse(localStorage.getItem('coords'))
-    setMyLocation({lat: parseFloat(coordsByLS.latitude), lng: parseFloat(coordsByLS.longitude)})
+    setMyLocation({ lat: parseFloat(coordsByLS.latitude), lng: parseFloat(coordsByLS.longitude) })
+    buscaCoordenadasParadas()
     buscaCoordenadasOnibus()
   }, [])
 
@@ -39,7 +53,7 @@ function AcompanhamentoOnibus() {
     {isLoaded ? (
       <GoogleMap
         mapContainerStyle={{ width: '100vw', height: '100vh' }}
-        center={myLocation}
+        center={coordenadasOnibus[0]}
         zoom={15}
         options={{
           streetViewControl: false,
@@ -48,18 +62,31 @@ function AcompanhamentoOnibus() {
           mapTypeControl: false
         }}
       >
-        { /* Child components, such as markers, info windows, etc. */}
-        <Marker position={myLocation}/>
-        {console.log(myLocation)}
+        <Marker position={myLocation} />
 
         {coordenadasOnibus.map(coords => (
           <Marker position={coords} icon={{
             url: myBusIcon,
             scaledSize: new google.maps.Size(25, 25), // scaled size
-            origin: new google.maps.Point(0,0), // origin
+            origin: new google.maps.Point(0, 0), // origin
             anchor: new google.maps.Point(0, 0) // anchor
-          }}/>
+          }} />
         ))}
+
+        {coordenadasParadas.map(coords => (
+          <Marker position={coords} icon={{
+            url: myBusStop,
+            scaledSize: new google.maps.Size(40, 20), // scaled size
+            origin: new google.maps.Point(0, 0), // origin
+            anchor: new google.maps.Point(0, 10) // anchor
+          }} />
+        ))}
+
+        <Polyline
+          options={{
+            path: coordenadasParadas
+          }}
+        />
         <></>
       </GoogleMap>
     ) : <></>}
